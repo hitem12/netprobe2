256c62e4f0368d49890247755bc90c6f64369815
Tomasz Pniewski-Przygodzki <t.pniewski.przygodzki@gmail.com>
2026-05-12 17:44:57 +0000

    Prototype AF_PACKET raw socket capture

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <CLI/CLI.hpp>
 #include <spdlog/spdlog.h>
 #include "logger.hpp"
-
+#include "dummy_af.hpp"
 int main(int argc, char** argv) {
     // Initialize logger
     auto logger = Logger::get();
+
     logger->info("NetLearn application started");
 
     // Setup CLI
@@ -13,11 +14,13 @@ int main(int argc, char** argv) {
     std::string interface;
     app.add_option("-i,--interface", interface, "Network interface to capture on");
 
-    int packet_count = 0;
+    int packet_count = 1;
     app.add_option("-c,--count", packet_count, "Number of packets to capture");
 
     bool verbose = false;
     app.add_flag("-v,--verbose", verbose, "Enable verbose output");
+    bool z = false;
+    app.add_option("-z",z,  "zupa");
 
     CLI11_PARSE(app, argc, argv);
 
@@ -27,13 +30,16 @@ int main(int argc, char** argv) {
     }
 
     if (interface.empty()) {
-        logger->warn("No interface specified, using default: eth0");
-        interface = "eth0";
+        interface = "wlan0";
+    }
+    logger->info("Interface: {}", interface);
+    auto status = dummy_af::ala(interface, packet_count);
+    if (!status)
+    {
+        logger->error("Failed to initialize: {}", status.error().message());
     }
 
-    logger->info("Interface: {}", interface);
     logger->info("Packet count: {}", packet_count);
-    logger->info("Application finished successfully");
 
     return 0;
 }
