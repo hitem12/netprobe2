39ce1c0370ab851dc344a36e592413031a8385c2
Tomasz Pniewski-Przygodzki <t.pniewski.przygodzki@gmail.com>
2026-09-08 19:43:07 +0200

    Simplify Epoll::add and move Epoll::create out of the header

--- src/Epoll.h
+++ src/Epoll.h
@@ -15,16 +15,8 @@ class Epoll
     int epfd_ {-1};
     explicit Epoll(int epfd) noexcept: epfd_(epfd) { }
 public:
-    static std::expected<Epoll, std::error_code> create() noexcept
-    {
-        int fd = ::epoll_create1(EPOLL_CLOEXEC);
-        if (fd == -1)
-        {
-            return std::unexpected(std::error_code(errno, std::system_category()));
-        }
-        return Epoll{fd};
-    }
-    ~Epoll() {if (epfd_ != -1) close(epfd_);}
+    static std::expected<Epoll, std::error_code> create() noexcept;
+ ~Epoll() {if (epfd_ != -1) close(epfd_);}
     Epoll(const Epoll&) = delete;
     Epoll& operator=(const Epoll&) = delete;
     Epoll(Epoll&& o) noexcept : epfd_(o.epfd_) { o.epfd_ = -1; }
@@ -32,7 +24,7 @@ public:
         if (this != &o) { if (epfd_ >= 0) ::close(epfd_); epfd_ = o.epfd_; o.epfd_ = -1; }
         return *this;
     }
-    std::error_code add(const int fd,const uint32_t events);
+    std::error_code add(const int fd);
     int wait(epoll_event* events, const int max, const int timeout);
 };
 
